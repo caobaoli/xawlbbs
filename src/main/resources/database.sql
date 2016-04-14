997243b50054f7277137cda221d0e9cb1c4c8234
caobaoli <caobaoli_it@126.com>
2016-04-14 23:04:13 +0800

    update4

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,16 +1,4 @@
--- --------------------------------------------------------
--- 主机:                           spring-abc.xyz
--- 服务器版本:                        5.5.43-MariaDB - MariaDB Server
--- 服务器操作系统:                      Linux
--- HeidiSQL 版本:                  9.3.0.4984
--- --------------------------------------------------------
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
--- 导出 spring_winds 的数据库结构
 CREATE DATABASE IF NOT EXISTS `spring_winds` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `spring_winds`;
 
